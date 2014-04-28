5910c6837b14b871b1181429f060ddb642f67ebf
david deraedt <dehats@gmail.com>
2014-04-28 13:48:17 +0200

    fixed issues with paths

--- createext.sh
+++ createext.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-#move to script dir parent
-BASEDIR=$(dirname $0)
-cd $BASEDIR
-cd ../
+BASEDIR=$(dirname "$0")
+EXTDIR=~/Library/Application\ Support/Adobe/CEPServiceManager4/extensions
 
 #create extensions folder if does not exist
-mkdir -p ~/Library/Application\ Support/Adobe/CEPServiceManager4/extensions
+mkdir -p $EXTDIR
 
 #copy template specified in 1st arg to destination specified in 2nd arg
-cp -r $BASEDIR/templates/$1/ ~/Library/Application\ Support/Adobe/CEPServiceManager4/extensions/$2
+cp -r "$BASEDIR/templates/$1/" "$EXTDIR/$2"
 
 #return resulting path
-echo ~/Library/Application\ Support/Adobe/CEPServiceManager4/extensions/$1
+echo "$EXTDIR/$2"
